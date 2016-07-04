b046e3edd9607e887c5b49b0bb157bc277f18ba2
Eric Fincken <eric.fincken@gmail.com>
2016-07-03 22:24:55 -0400

    created files and output variable

---
+++ MyfirstApplication/src/myfirstapplication/MyfirstApplication.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myfirstapplication;
+
+/**
+ *
+ * @author Fincken
+ */
+public class MyfirstApplication {
+    String output = "Hello World";
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
