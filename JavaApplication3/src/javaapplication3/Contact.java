f695632153ecafbd47731ae4ed73f8be227d4eb2
Eric Fincken <eric.fincken@gmail.com>
2016-07-08 17:51:03 -0400

    set up phone book program

---
+++ JavaApplication3/src/javaapplication3/Contact.java
@@ -0,0 +1,23 @@
+package javaapplication3;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Fincken
+ */
+public class Contact {
+    String firstName;
+    String lastName;
+    String homePhone;
+    String cellPhone;
+    String workPhone;
+    
+    Contact(){
+        firstName = lastName = homePhone = cellPhone = workPhone = "";
+    }
+}
